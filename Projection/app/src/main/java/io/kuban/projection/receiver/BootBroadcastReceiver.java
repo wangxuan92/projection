61197521ef86d89772b33ac3d16fc8d397fc7685
wangxuan <wangxuan@wangxuandeMacBook-Pro.local>
2018-07-17 18:29:03 +0800

    添加自启

--- Projection/app/src/main/java/io/kuban/projection/receiver/BootBroadcastReceiver.java
+++ Projection/app/src/main/java/io/kuban/projection/receiver/BootBroadcastReceiver.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.util.Log;
 
 import io.kuban.projection.activity.LogInActivity;
+import io.kuban.projection.activity.MainActivity;
 import io.kuban.projection.base.ActivityManager;
 
 /**
@@ -17,8 +18,25 @@ public class BootBroadcastReceiver extends BroadcastReceiver {
     /**
      * demo2: 可以实现开机自动打开软件并运行。
      */
+//    @Override
+//    public void onReceive(Context context, Intent intent) {
+//        ActivityManager.toLogInActivity(context);
+//    }
+    static final String action_boot ="android.intent.action.BOOT_COMPLETED";
+
+
     @Override
-    public void onReceive(Context context, Intent intent) {
-        ActivityManager.toLogInActivity(context);
+
+    public void onReceive (Context context, Intent intent) {
+
+        Log.i("charge completed", "启动完成");
+
+        if (intent.getAction().equals(action_boot)){
+
+            Intent mBootIntent = new Intent(context, MainActivity.class);
+            // 下面这句话必须加上才能开机自动运行app的界面
+            mBootIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(mBootIntent);
+        }
     }
 }
